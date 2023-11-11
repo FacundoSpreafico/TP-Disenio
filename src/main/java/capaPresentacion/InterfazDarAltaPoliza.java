8e6985e5dcf4b0c12694b630fd1d1430afae05c9
FacundoSpreafico <fspreafico@frsf.utn.edu.ar>
2023-11-11 17:18:30 -0300

    actInterfaz

--- src/main/java/capaPresentacion/InterfazDarAltaPoliza.java
+++ src/main/java/capaPresentacion/InterfazDarAltaPoliza.java
@@ -11,25 +11,6 @@ public class InterfazDarAltaPoliza extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					InterfazDarAltaPoliza frame = new InterfazDarAltaPoliza();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public InterfazDarAltaPoliza() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);

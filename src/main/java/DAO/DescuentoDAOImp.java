8b70e48de37f3097bb091b40daaf11d00cbc4832
Santiago <santiago.wangher@gmail.com>
2023-12-05 18:55:09 -0300

    agregado porcentaje descuento

---
+++ src/main/java/DAO/DescuentoDAOImp.java
@@ -0,0 +1,19 @@
+package DAO;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import entidades.DescuentoPorUnidadAdicional;
+import entidades.HistorialDescuentoPorUnidadAdicional;
+
+public class DescuentoDAOImp implements DescuentoDAO {
+	public HistorialDescuentoPorUnidadAdicional getPorcentajeActual(String cantUnidades) {
+		try (Session session = SessionHibernate.getInstance().getSessionFactory().openSession()) {
+			String hql = "FROM DescuentoPorUnidadAdicional WHERE cantUnidades = :cantUnidades";
+			Query<DescuentoPorUnidadAdicional> query = session.createQuery(hql,DescuentoPorUnidadAdicional.class);
+			query.setParameter("cantUnidades", cantUnidades);
+			DescuentoPorUnidadAdicional porcentaje = query.getSingleResult();
+			return porcentaje.getValorActual();	
+			}
+	}
+}
